d1b9284745eb8dc7e250ce8d163f18fb51718ce1
david <david@va7eex.ca>
2020-09-21 07:45:51 +0000

    Further layout changes

--- labelmaker.py
+++ labelmaker.py
@@ -5,6 +5,10 @@ from simple_zpl2 import ZPLDocument
 from simple_zpl2 import Code128_Barcode
 from simple_zpl2 import QR_Barcode
 
+def wraptext_big(text):
+    if len(text) > 20:
+        return text[:20] + '\n' + text[20:]
+
 def printlabel(bigtext, littletextleft='', littletextright='', barcodedata='', barcodetype='code128'):
 
     prn = NetworkPrinter(os.getenv('PRINTER_IP'))
@@ -12,22 +16,21 @@ def printlabel(bigtext, littletextleft='', littletextright='', barcodedata='', b
 
     zpl.add_field_origin(10, 10)
     zpl.add_font('C', zpl._ORIENTATION_NORMAL, 30)
-    zpl.add_field_data(bigtext)
+    zpl.add_field_data(wraptext_big(bigtext))
 
-    zpl.add_field_origin(0, 40, justification='0')
+    zpl.add_field_origin(0, 60, justification='0')
     zpl.add_font('C', zpl._ORIENTATION_NORMAL, 15)
     zpl.add_field_data(littletextleft)
 
-    zpl.add_field_origin(400, 40, justification='1')
+    zpl.add_field_origin(450, 60, justification='1')
     zpl.add_font('C', zpl._ORIENTATION_NORMAL, 15)
     zpl.add_field_data(littletextright)
 
+    zpl.add_field_origin(20, 90)
     if 'code128' in barcodetype:
-        zpl.add_field_origin(20, 20)
         bc = Code128_Barcode(barcodedata, 'N', 30, 'Y')
         zpl.add_barcode(bc)
     elif 'qr' in barcodetype:
-        zpl.add_field_origin(20, 20)
         bc = QR_Barcode(barcodedata, 'N', 30, 'Y')
         zpl.add_barcode(bc)
 
