c78e8a7fe318fe53d636395d45dda20194a1934c
DR <25559687+va7eex@users.noreply.github.com>
2021-08-28 17:08:36 -0700

    correcting object type

--- discordbot.py
+++ discordbot.py
@@ -62,7 +62,7 @@ async def on_message(message):
         report = lp.printlabel(labelstr[0],labelstr[1].strip(),labelstr[2].strip(),labelstr[3].strip(),labelstr[4].strip(), quantity=qty, preview=preview)
         if type(report) is bool:
             await message.channel.send(f'Printing label x {qty}:\n\t{labelstr}')
-        elif type(report) is bytearray:
+        elif type(report) is bytes:
             await message.channel.send(file=Image.open(io.BytesIO(report)))
 
 def osBooltoPyBool(str):
